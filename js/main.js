88cb23692d456bec9eb851d28a1871625421075f
craigpullar <craigmichaelpullar@gmail.com>
2019-04-30 22:04:04 -0400

    Turned observer into a dom node creator

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const model = NumberModel();
-const elementObserver1 = ElementObserver("elements-observer-1");
+const elementObserver1 = SquareObserver("elements-observer-1");
+const elementObserver2 = SquareObserver("elements-observer-2");
 const consoleObserver1 = ConsoleObserver();
 const historyObserver1 = HistoryObserver();
 
 model.addObserver(elementObserver1);
+model.addObserver(elementObserver2);
 model.addObserver(consoleObserver1);
 model.addObserver(historyObserver1);
 model.notifyObservers();
